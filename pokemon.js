30e5aed131ff43bb7b317b2d09c1c6484fa713a3
Adem <ado.designs@outlook.de>
2025-05-07 15:00:51 +0200

    Added new functions to display Pokémon cards

--- pokemon.js
+++ pokemon.js
@@ -1,28 +1,38 @@
 // https://pokeapi.co/api/v2/pokemon?limit=100&offset=0
 let offset = 0;
 
-
 async function loadPokemons() {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`);
+    const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`
+    );
+
     const data = await response.json();
     const allPokemons = data.results;
 
-    for (let i = 0; i < allPokemons.length; i++) {
-        const pokemonURL = allPokemons[i].url;
-        const response = await fetch(pokemonURL);
-        const data = await response.json();
+    await renderPokemonList(allPokemons);
+}
 
-        const id = data.id;
-        const name = data.name.toUpperCase();
-        const image = data.sprites.other['official-artwork'].front_default;
-        const type1 = data.types[0].type.name;
-        const type2 = data.types[1] ? data.types[1].type.name : '';
+loadPokemons();
 
-        const cardHTML = renderPokemonCards(id, name, image, type1, type2);
-        document.getElementById('content').innerHTML += cardHTML;
+async function renderPokemonList(pokemonList) {
+    for (let i = 0; i < pokemonList.length; i++) {
+        await renderSinglePokemon(pokemonList[i].url);
     }
 }
-loadPokemons();
+async function renderSinglePokemon(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const id = data.id;
+    const name = data.name.toUpperCase();
+    const image = data.sprites.other['official-artwork'].front_default;
+    const type1 = data.types[0].type.name;
+    const type2 = data.types[1] ? data.types[1].type.name : '';
+
+    const cardHTML = renderPokemonCards(id, name, image, type1, type2);
+    document.getElementById('content').innerHTML += cardHTML;
+}
+
 
 function getBgClass(type) {
     return `bg-${type}`;
